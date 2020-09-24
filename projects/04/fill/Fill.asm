19f0b4c8aaf66d915667dd7b8e8c4b2097cde5bc
dorayakikun <github@dorayakikun.com>
2020-09-24 21:18:49 +0900

    Add Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @24575
+    D = A      // D = 24575
+
+    @0
+    M = D      // M[0] = 24575
+
+    @SCREEN
+    D = A      // D = @SCREEN Address
+    @1
+    M = D      // M[1] = @SCREEN Address
+(LOOP)
+    @KBD
+    D = M
+    @FILL
+    D;JGT      // if out > 0 jump
+
+    @CLEAR
+    0;JMP      // jump
+(FILL)
+    @0
+    D = M      // D = M[0]
+    @1
+    D = D - M  // D = M[0] - M[1]
+    @LOOP
+    D;JLT      // if out < 0 jump
+
+    @1
+    D = M      // D = M[1]
+    A = M      // A = M[1]
+    M = -1
+
+    @1
+    M = D + 1  // M = D + 1 
+
+    @LOOP
+    0;JMP      // jump
+(CLEAR)
+    @SCREEN
+    D = A      // D = @SCREEN Address
+    @1
+    D = D - M  // D = @SCREEN Address - M[1]
+    @LOOP
+    D;JGT      // if out > 0 jump
+
+    @1
+    D = M      // D = M[1]
+    A = M      // A = M[1]
+    M = 0      // M[1] = 0
+
+    @1
+    M = D - 1  // M[1] = M[1] - 1
+
+    @LOOP
+    0;JMP

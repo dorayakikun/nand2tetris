380fb8e58d9040cd7d89474e98fdb4c2cd901fd2
dorayakikun <github@dorayakikun.com>
2021-01-21 21:48:32 +0900

    wip

--- projects/07/vm_translater/src/code_writer.rs
+++ projects/07/vm_translater/src/code_writer.rs
@@ -5,125 +5,70 @@ const PUSH_ONTO_STACK: &str = r#"// push onto stack
 A=M
 M=D
 @SP
-M=M+1
-"#;
+M=M+1"#;
 
 pub fn write_code(file_name: &str, command: &Command) -> String {
     match command {
-        Command::Push(segment, index) => match segment {
-            Segment::Argument => {
-                format!(
-                    "{}\n{}\n{}",
-                    write_load_value_into_d(index),
-                    write_load_value_offset_into_d("ARG"),
-                    PUSH_ONTO_STACK
-                )
-            }
-            Segment::Local => {
-                format!(
-                    "{}\n{}\n{}",
-                    write_load_value_into_d(index),
-                    write_load_value_offset_into_d("LCL"),
-                    PUSH_ONTO_STACK
-                )
-            }
-            Segment::Static => {
-                format!(
-                    r#"@{}.{}
-D=M
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    file_name, index
-                )
-            }
-            Segment::Constant => {
-                format!(
-                    r#"@{}
-D=A
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    index
-                )
-            }
-            Segment::This => {
-                format!(
-                    r#"@{}
-D=A
-@THIS
-A=D+M
-D=M
+        Command::Push(segment, index) => write_code_push(file_name, segment, index),
+    }
+}
 
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    index
-                )
-            }
-            Segment::That => {
-                format!(
-                    r#"@{}
-D=A
-@THAT
-A=D+M
-D=M
-
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    index
-                )
-            }
-            Segment::Pointer => {
-                format!(
-                    r#"@{}
-D=A
-
-@{}
-A=D+A
-D=M
-
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    index, 3
-                )
-            }
-            Segment::Temp => {
-                format!(
-                    r#"@{}
-D=A
-
-@{}
-A=D+A
-D=M
-
-// push to stack
-@SP
-A=M
-M=D
-@SP
-M=M+1"#,
-                    index, 5
-                )
-            }
-        },
+fn write_code_push(file_name: &str, segment: &Segment, index: &i32) -> String {
+    match segment {
+        Segment::Argument => {
+            format!(
+                "{}\n{}\n{}",
+                write_load_value_into_d(index),
+                write_load_value_offset_into_d("ARG"),
+                PUSH_ONTO_STACK
+            )
+        }
+        Segment::Local => {
+            format!(
+                "{}\n{}\n{}",
+                write_load_value_into_d(index),
+                write_load_value_offset_into_d("LCL"),
+                PUSH_ONTO_STACK
+            )
+        }
+        Segment::Static => {
+            format!("@{}.{}\nD=M\n{}", file_name, index, PUSH_ONTO_STACK)
+        }
+        Segment::Constant => {
+            format!("{}\n{}", write_load_value_into_d(index), PUSH_ONTO_STACK)
+        }
+        Segment::This => {
+            format!(
+                "{}\n{}\n{}",
+                write_load_value_into_d(index),
+                write_load_value_offset_into_d("THIS"),
+                PUSH_ONTO_STACK
+            )
+        }
+        Segment::That => {
+            format!(
+                "{}\n{}\n{}",
+                write_load_value_into_d(index),
+                write_load_value_offset_into_d("THAT"),
+                PUSH_ONTO_STACK
+            )
+        }
+        Segment::Pointer => {
+            format!(
+                "{}\n@{}\nA=D+A\nD=M\n{}",
+                write_load_value_into_d(index),
+                3,
+                PUSH_ONTO_STACK
+            )
+        }
+        Segment::Temp => {
+            format!(
+                "{}\n@{}\nA=D+A\nD=M\n{}",
+                write_load_value_into_d(index),
+                5,
+                PUSH_ONTO_STACK
+            )
+        }
     }
 }
 
@@ -147,8 +92,7 @@ D=A
 @ARG
 A=D+M
 D=M
-
-// push to stack
+// push onto stack
 @SP
 A=M
 M=D

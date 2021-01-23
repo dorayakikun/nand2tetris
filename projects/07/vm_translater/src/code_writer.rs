664f6b5621d469e09a79def7549b2da638f63290
dorayakikun <github@dorayakikun.com>
2021-01-23 17:12:27 +0900

    wip

--- projects/07/vm_translater/src/code_writer.rs
+++ projects/07/vm_translater/src/code_writer.rs
@@ -1,15 +1,31 @@
 use crate::{command::Command, segment::Segment};
 
-const PUSH_ONTO_STACK: &str = r#"// push onto stack
+const PUSH_ONTO_STACK: &str = r#"// Push value onto stack
 @SP
 A=M
 M=D
 @SP
 M=M+1"#;
 
+const POP_STACK_INTO_D_AS_ADDR: &str = r#"// Write d in a general register
+@R13
+M=D
+
+// Decrese stack pointer and load value into d
+@SP
+AM=M-1
+D=M
+
+// Jump to general register and write d in it
+@R13
+A=M
+M=D"#;
+
 pub fn write_code(file_name: &str, command: &Command) -> String {
     match command {
+        Command::Arithmetic(arithmetic_command) => { unimplemented!() },
         Command::Push(segment, index) => write_code_push(file_name, segment, index),
+        Command::Pop(segment, index) => write_code_pop(file_name, segment, index),
     }
 }
 
@@ -55,17 +71,31 @@ fn write_code_push(file_name: &str, segment: &Segment, index: &i32) -> String {
         }
         Segment::Pointer => {
             format!(
-                "{}\n@{}\nA=D+A\nD=M\n{}",
+                r#"{}
+
+// offset by 3
+// pointer i は 3 + i 番目のアドレスへ 変換されるべき
+@3
+A=D+A
+D=M
+
+{}"#,
                 write_load_value_into_d(index),
-                3,
                 PUSH_ONTO_STACK
             )
         }
         Segment::Temp => {
             format!(
-                "{}\n@{}\nA=D+A\nD=M\n{}",
+                r#"{}
+
+// offset by 5
+// temp i は 5 + i 番目のアドレスへ 変換されるべき
+@5
+A=D+A
+D=M
+
+{}"#,
                 write_load_value_into_d(index),
-                5,
                 PUSH_ONTO_STACK
             )
         }
@@ -73,11 +103,89 @@ fn write_code_push(file_name: &str, segment: &Segment, index: &i32) -> String {
 }
 
 fn write_load_value_into_d(value: &i32) -> String {
-    format!("@{}\nD=A", value)
+    format!(r#"// Load constant or offset into d
+@{}
+D=A"#, value)
 }
 
 fn write_load_value_offset_into_d(segment: &str) -> String {
-    format!("@{}\nA=D+M\nD=M", segment)
+    format!(r#"// Load value offset and Load offset + address into d
+@{}
+A=D+M
+D=M"#, segment)
+}
+
+fn write_code_pop(file_name: &str, segment: &Segment, index: &i32) -> String {
+    match segment {
+        Segment::Argument => {
+            write_pop_stack_into_segment("ARG", index)
+        }
+        Segment::Local => {
+            write_pop_stack_into_segment("LCL", index)
+        }
+        Segment::Static => {
+            format!(r#"{}.{}
+D=A
+
+{}"#, file_name, index, POP_STACK_INTO_D_AS_ADDR)
+        }
+        Segment::Constant => {
+            unimplemented!()
+        }
+        Segment::This => {
+            write_pop_stack_into_segment("THIS", index)
+        }
+        Segment::That => {
+            write_pop_stack_into_segment("THAT", index)
+        }
+        Segment::Pointer => {
+            format!(r#"// Load a base address into d
+{}
+D=A
+
+// base + 3
+@3
+D=D+A
+
+{}"#, index, POP_STACK_INTO_D_AS_ADDR)
+        }
+        Segment::Temp => {
+            format!(r#"// Load a base address into d
+{}
+D=A
+
+// base + 5
+@5
+D=D+A
+
+{}"#, index, POP_STACK_INTO_D_AS_ADDR)
+        }
+    }
+}
+
+fn write_pop_stack_into_segment(segment: &str, index: &i32) -> String {
+    format!(r#"// Load offset value into d
+@{}
+D=A
+
+// Jump to segment + offset
+@{}
+D=M+D
+
+// Assign address to temp
+// R13 - 15 は汎用的なレジスタとしてVM実装で用いることができる
+@R13
+M=D
+
+// Decrese stack pointer and pop stack into d
+@SP
+AM=M-1
+D=M
+
+// Jump to temp and write d in it
+@R13
+A=M
+M=D"#, index, segment).to_string()
 }
 
 mod tests {
--- projects/07/vm_translater/src/command.rs
+++ projects/07/vm_translater/src/command.rs
@@ -4,10 +4,4 @@ pub enum Command {
     Arithmetic(ArithmeticCommand),
     Push(Segment, i32),
     Pop(Segment, i32),
-    Label(String, String),
-    Goto(String, String),
-    IfGoto(String, String),
-    DefineFunction(String, i32),
-    CallFunction(String, i32),
-    Return,
 }

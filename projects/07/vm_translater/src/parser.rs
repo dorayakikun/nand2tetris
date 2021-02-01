2dbdec38ce697081eed77d36632484098e53b56f
dorayakikun <github@dorayakikun.com>
2021-02-01 23:21:51 +0900

    wip

--- projects/07/vm_translater/src/parser.rs
+++ projects/07/vm_translater/src/parser.rs
@@ -1,1 +1,5 @@
+use crate::command::Command;
 
+pub fn parse(file_name: &str) -> Result<Vec<Command>, std::io::Error> {
+    Ok(vec![Command::Arithmetic(crate::arithmetic_command::ArithmeticCommand::Add)])
+}

769938d23e40bb4a495cec4cd17bd2c574ab1848
dorayakikun <github@dorayakikun.com>
2021-02-03 22:38:22 +0900

    wip

--- projects/07/vm_translater/src/parser.rs
+++ projects/07/vm_translater/src/parser.rs
@@ -1,5 +1,21 @@
 use crate::command::Command;
-
+use std::{fs::File, ops::Index};
+use std::io::{self, BufRead};
 pub fn parse(file_name: &str) -> Result<Vec<Command>, std::io::Error> {
+    let lines = read_lines(file_name)?;
+    for line in lines {
+        let line = line?;
+        if line.starts_with("//") {
+            continue;
+        }
+        if let Some(index) = line.find("//") {
+            
+        }
+    }
     Ok(vec![Command::Arithmetic(crate::arithmetic_command::ArithmeticCommand::Add)])
+}
+
+fn read_lines(file_name: &str) -> Result<io::Lines<io::BufReader<File>>, io::Error> {
+    let file = File::open(file_name)?;
+    Ok(io::BufReader::new(file).lines())
 }

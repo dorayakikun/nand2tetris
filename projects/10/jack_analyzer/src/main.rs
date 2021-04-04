1e8d5283613f83fc3f08adcce786022348d419bc
dorayakikun <github@dorayakikun.com>
2021-04-04 21:07:26 +0900

    wip

--- projects/10/jack_analyzer/src/main.rs
+++ projects/10/jack_analyzer/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, path::Path};
+mod errors;
+fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err(errors::JackAnalyzerError::InvalidArgumentLength(args.len()));
+    }
+    let path_dir = Path::new(&args[1]);
+    if path_dir.is_dir() {
+    } else {
+    }
+    Ok(())
 }
